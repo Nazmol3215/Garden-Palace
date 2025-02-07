f0cd34378b3b99d3289a9c6a3bb444f4497bc0e1
Nazmol3215 <mdasikahamad2006@gmail.com>
2025-02-07 19:23:15 +0600

    Four commit

--- src/Components/PorductDetails.jsx
+++ src/Components/PorductDetails.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import picture from "../Images/i2png.jpg";
-import picture1 from "../Images/i3.png";
-import picture2 from "../Images/i7.pg.jpg";
+import picture from "../Images/matel.png";
+import picture1 from "../Images/ma.png.jpeg";
+import picture2 from "../Images/ba.png";
 import "./ProductCard.css";
 
 
@@ -9,37 +9,37 @@ import "./ProductCard.css";
 // প্রোডাক্টের ডাটা
 const products = [
   {
-    id: 1,
-    image:picture,
-    title: "প্রয়োজনীয় মেটাল রেক, বীজ, সার, ",
-    description: "মাদের স্টকে চলে এসেছে জৈব ও ভার্মি মিক্সড পায়েল সার খুচরা প্রতি কেজি ৪০৳ | আপনার বারান্দা কিংবা ছাদে যে কোন ধরনের মেটাল রেক কাস্টমাইজড করে নিতে যোগাযোগ করুন ইনবক্সে অ",
-    rating: "★★★★☆",
-    sold: "2.1K+ bought in past month",
-    price: "499.99 ৳",
-    oldPrice: "699.99 ৳",
-    deliveryDate: "Tue, Nov 15",
+      id: 1,
+      image: picture,
+      title: "মেটাল সাইকেল প্লান্ট স্ট্যান্ড",
+      description: `মেটাল সাইকেল প্লান্ট স্ট্যান্ড 
+    লম্বা - ২৪", পাশে - ২৮"
+    টব বসানোর স্পেস ৫"/৬"/৭",
+    মজবুত ও প্লাস্টিক কোটিং রং করা, মরিচা ধরবে না।
+    দারুন সব ইনডোর প্লান্ট দিয়ে সাজিয়ে নিতে পারেন।`,
+      sold: "100+ bought in past month",
+      price: "অফার মূল্য - ৯৯৯৳",
+      oldPrice: 699.99,
+    
   },
   {
     id: 2,
     image: picture1,
-    title: "প্রয়োজনীয় মেটাল রেক, বীজ, সার, ",
-    description: "মাদের স্টকে চলে এসেছে জৈব ও ভার্মি মিক্সড পায়েল সার খুচরা প্রতি কেজি ৪০৳ | আপনার বারান্দা কিংবা ছাদে যে কোন ধরনের মেটাল রেক কাস্টমাইজড করে নিতে যোগাযোগ করুন ইনবক্সে অ।",
-    rating: "★★★★★",
-    sold: "3K+ bought in past month",
-    price: "299.99 ৳",
-    oldPrice: "399.99 ৳",
-    deliveryDate: "Wed, Nov 16",
+    title: "মেটাল বেলকনি রেক, ",
+    description: `সাদা ১৫"/২৬"/৩৬" - টব বসানোর স্পেস ৮" 
+৩ পিসের প্যাকেজ,  বেলকনি রেক গুলো যথেষ্ট মজবুত ও প্লাস্টিক কোটিং রং করা,  সহজেই ১০"-১২" যে কোন ভারী টব বসানো যাবে,  সাথে ১০টি টব ফ্রি দেওয়া হবে।`,
+    sold: "100+ bought in past month",
+    price: " অফার মূল্য 299.99 ৳",
+    oldPrice: " - ১৮৫০৳",
   },
   {
     id: 3,
     image: picture2,
-    title: "প্রয়োজনীয় মেটাল রেক, বীজ, সার, ",
-    description: "মাদের স্টকে চলে এসেছে জৈব ও ভার্মি মিক্সড পায়েল সার খুচরা প্রতি কেজি ৪০৳ | আপনার বারান্দা কিংবা ছাদে যে কোন ধরনের মেটাল রেক কাস্টমাইজড করে নিতে যোগাযোগ করুন ইনবক্সে অ",
-    rating: "★★★★☆",
-    sold: "1.9K+ bought in past month",
-    price: "626.84 ৳",
+    title: "ব্রাজিলিয়ান লাকি উড",
+    description: "ইনডোর প্লান্ট / ডেস্ক প্লান্ট আপনার টেবিলে দারুণ এই ইনডোর গাছটি রাখতে পারবেন,  শুধুমাত্র পাথর ও পানিতে রাখা যাবে, ৪-৫ দিন পরপর পানি চেঞ্জ করতে হবে",
+    sold: "119+ bought in past month",
+    price: "টব সহ মূল্য - ৩৫০৳",
     oldPrice: "799.99 ৳",
-    deliveryDate: "Thu, Nov 17",
   },
 ];
 
@@ -55,15 +55,12 @@ const ProductCard = () => {
                 <h5 className="card-title text-primary fw-bold">{product.title}</h5>
                 <p className="card-text text-muted flex-grow-1">{product.description}</p>
                 <div className="d-flex align-items-center mb-2">
-                  <span className="text-warning me-2">{product.rating}</span>
                   <small className="text-muted">{product.sold}</small>
                 </div>
-                <h4 className="text-danger fw-bold" style={{ color: 'red', fontSize: '29px', marginRight: '5px' }}>{product.price}</h4>
+                <h4 className="text-danger fw-bold" style={{ color: 'red', fontSize: '19px', marginRight: '5px' }}>{product.price}</h4>
                 <p className="text-muted">
                   <del style={{ color: 'back', fontSize: '24px', marginRight: '5px' }}>List: {product.oldPrice}</del>
                 </p>
-                <p className="text-success">Delivery {product.deliveryDate}</p>
-                <p className="text-muted">Ships to Addass</p>
                 <button className="btn btn-primary w-100 mt-auto" style={{ backgroundColor: '#3F9E53' }}>Order Now</button>
               </div>
             </div>
@@ -75,3 +72,4 @@ const ProductCard = () => {
 };
 
 export default ProductCard;
+
